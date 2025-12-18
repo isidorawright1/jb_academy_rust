7d41d91681d5c01c9b3db09d9682a5e88dcb894c
isidora wright <isidora.wright.1@gmail.com>
2025-12-17 19:18:14 -0700

    Tuples and Arrays Complete

--- Common Programming Concepts/Variables, Functions, and Conditions Test/Calculate Price/src/lib.rs
+++ Common Programming Concepts/Variables, Functions, and Conditions Test/Calculate Price/src/lib.rs
@@ -1,3 +1,13 @@
-pub fn calculate_price(/* TODO */) /*TODO*/ {
-    /*write your solution here*/
+pub fn calculate_price(num_apples :i32) -> i32 {
+    if num_apples >= 40 {
+        num_apples
+    }
+    else {
+        num_apples * 2
+    }
+}
+
+fn main() {
+    let price = calculate_price(5);
+    println!("The price of apples is: {}", price);
 }
