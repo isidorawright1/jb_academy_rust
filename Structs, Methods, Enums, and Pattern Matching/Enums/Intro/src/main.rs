55db92dcf3f16644c277d4e347fe6afadb4fb053
Isidora Wright <isidora.wright.1@gmail.com>
2026-03-22 15:27:40 -0600

    enum practice

--- Structs, Methods, Enums, and Pattern Matching/Enums/Intro/src/main.rs
+++ Structs, Methods, Enums, and Pattern Matching/Enums/Intro/src/main.rs
@@ -1,3 +1,45 @@
+enum Shape {
+    Circle(f64),
+    Rectangle(f64, f64),
+    Square(f64),
+}
+
+impl Shape {
+    fn area(&self) -> f64 {
+        match self {
+            Shape::Circle(radius) => std::f64::consts::PI * (radius * radius),
+            Shape::Rectangle(length, height) => length * height,
+            Shape::Square(side) => side * side,
+        }
+    }
+
+    fn get_info(&self) -> String {
+        match self {
+            Shape::Circle(radius) => {
+                format!("This object is a circle with a radius of {:.2}", radius)
+            },
+            Shape::Rectangle(length, height) => {
+                format!("This object is a rectangle with a length of {:.2} and width of {:.2}", length, height)
+            },
+            Shape::Square(side) => {
+                format!("This object is a square with a side length of: {:.2} \n", side)
+            }
+        }
+    }
+}
+
+//practice with emus - empty, unit, tuple, struct, etc.
+
 fn main() {
-    // put your code here to launch it
+    let circle = Shape::Circle(10.0);
+
+    println!("The area of your circle is: {:.2}", circle.area());
+    println!("{}", circle.get_info());
+
+    let rect = Shape::Rectangle(3.0, 2.0);
+    println!("The area of your rectangle is: {:.2}", rect.area());
+    println!("{}", rect.get_info());
+
+    let square = Shape::Square(5.0);
+    println!("{}", square.get_info());
 }
